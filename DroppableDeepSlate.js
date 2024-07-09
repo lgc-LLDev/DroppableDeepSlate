eedff7f9972106606d645e1c77239550c0b6fccf
student_2333 <lgc2333@126.com>
2024-07-09 17:02:31 +0800

    change code style

--- DroppableDeepSlate.js
+++ DroppableDeepSlate.js
@@ -8,9 +8,9 @@ mc.listen('onDestroyBlock', (player, block) => {
     player.getHand().type === 'minecraft:netherite_pickaxe' &&
     block.type === 'minecraft:reinforced_deepslate'
   ) {
-    const it = mc.newItem('minecraft:reinforced_deepslate', 1);
-    if (it) mc.spawnItem(it, block.pos);
+    const it = mc.newItem('minecraft:reinforced_deepslate', 1)
+    if (it) mc.spawnItem(it, block.pos)
   }
-});
+})
 
-ll.registerPlugin('DroppableDeepSlate', '让强化深板岩掉落物品', [0, 1, 1], {});
+ll.registerPlugin('DroppableDeepSlate', '让强化深板岩掉落物品', [0, 1, 1], {})
